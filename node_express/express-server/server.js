72ace198a4548f73723a28d7f81a76a92235d10b
gauravxdhingra <grvdhingra1999@gmail.com>
2021-01-03 11:25:47 +0530

    Calc - Init

--- node_express/express-server/server.js
+++ node_express/express-server/server.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at: grvdhingra1999@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("I am Gaurav. I'm a programmer!");
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000"); 
+});
