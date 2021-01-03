fc86e0b9d110a0cc31a3248ead1a48775c3d63ca
gauravxdhingra <grvdhingra1999@gmail.com>
2021-01-03 11:03:08 +0530

    Express and Node - Init

--- intro/dom_intermediate/drum_roll_kit/index.js
+++ intro/dom_intermediate/drum_roll_kit/index.js
@@ -2,15 +2,11 @@ var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < noOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        // console.log(this.innerHTML);
-        // var audio = new Audio("sounds/tom-1.mp3");
-        // audio.play();
         var keyPressed = this.innerHTML;
         makeSound(keyPressed);
         buttonAnimation(keyPressed);
     });
 }
-
 
 document.addEventListener("keypress", function (event) {
     makeSound(event.key);
